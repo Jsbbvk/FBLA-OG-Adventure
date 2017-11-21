5c7a568dd7734b898b768a2decba7edd26c37432
Jsbbvk <Jsbbvk@gmail.com>
2017-11-21 09:19:53 -0800

    added 3D World

---
+++ 3D World/Assets/Assets/Scripts/ObjectivePlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivePlayerController : MonoBehaviour {
+    protected CharacterController controller;
+    public GameObject[] gameObj;
+    private bool isInteractable = false;
+    public float inBoundOfInteractions;
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        gameObj = GameObject.FindGameObjectsWithTag("Objective");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject g in gameObj)
+        {
+            Transform target = g.GetComponent<Transform>();
+
+            var heading = target.position - GetComponent<Transform>().position;
+            heading.y = 0;
+
+            float offset = g.GetComponent<Collider>().bounds.size.x * target.localScale.x;
+            //Debug.Log(offset);
+            if (heading.sqrMagnitude <= inBoundOfInteractions + offset*1.5)
+            {
+                isInteractable = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    //Interaction
+                    //such as pickup or open a menu
+                    //TextBoxManager.SetText("ASDF");
+                    TextBoxManager t = GameObject.Find("Text Box Manager").GetComponent<TextBoxManager>();
+                    t.SetText("testText");
+                }
+            } else
+            {
+                isInteractable = false;
+            }
+           // if (heading.sqrMagnitude < 50) Debug.Log(heading.sqrMagnitude);
+        }
+	}
+
+    private void OnGUI()
+    {
+        if (isInteractable)
+        {
+            GUI.Box(new Rect(Screen.width/2-90/2, Screen.height - 25, 90, 25), ("Interact [E]"));
+        }
+    }
+}
---
+++ 3D World/README.txt
@@ -0,0 +1,14 @@
+IMPORTANT INSTRUCTIONS
+Edit the world in the scene WorldEditHere.
+
+could make a new scene and copy MainPlayer into it.
+
+All the materials are in the Urban folder - /Urban/Prefab/.  Drag whatever object you want into the scene window.  
+MAKE SURE that the mesh collider is active.  
+You can do this if you go into /Scripts/ and drag the AddMeshCollider script to the object in the 
+Hierarchy tab (it should add a meshcollider when you run the scene).
+
+To change the color of an object, go to /Urban/Models/Materials/ and drag whatever color you want onto the object
+in the Hierarchy tab.
+
+Have fun Designing!
