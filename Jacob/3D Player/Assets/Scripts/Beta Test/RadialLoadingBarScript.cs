d2554c04c3c68428f3adaa366f2ef515a3f4ce82
Jsbbvk <Jsbbvk@gmail.com>
2018-01-02 22:07:25 -0800

    added a 'repairable' object for a minigame

---
+++ Jacob/3D Player/Assets/Scripts/Beta Test/RadialLoadingBarScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RadialLoadingBarScript : MonoBehaviour {
+    public Image Center;
+    //this is the ProgressBar GameObject under 'Radial Loading Bar Canvas'
+    public GameObject ProgressBar;
+    public float timer = 0;
+    public bool heldDown = false;
+    public float loadSpeed = 1;
+
+    public GameObject obj;
+    // Update is called once per frame
+    void Update () {
+		if (ProgressBar.activeSelf && Input.GetKeyDown(KeyCode.E))
+        {
+            heldDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            heldDown = false;
+            timer = 0;
+            Center.fillAmount = 0;
+        }
+
+        if (heldDown)
+        {
+            timer++;
+            Center.fillAmount = timer*loadSpeed / 100;
+        }
+        if (Center.fillAmount == 1)
+        {
+            FinishLoading();
+        }
+    }
+
+    public void FinishLoading()
+    {
+        obj.GetComponent<RepairableObject>().FinishLoading();
+        gameObject.SetActive(false);
+        timer = 0;
+        heldDown = false;
+    }
+
+    public void SetGameObjectToCall(GameObject g)
+    {
+        obj = g;
+    }
+}
---
+++ Jacob/3D Player/Assets/Scripts/Beta Test/RepairableObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairableObject : MonoBehaviour {
+    public Transform Player;
+    public float inBoundOfInteractions;
+
+    public bool isRepaired = false;
+    public GameObject RadialLoadingBar;
+
+    public void FinishLoading()
+    {
+        //sparkle animation
+        Debug.Log("YAY!");
+        isRepaired = true;
+    }
+
+    void Start()
+    {
+        Player = GameObject.Find("Main Player").GetComponent<Transform>();
+        inBoundOfInteractions = 10f;
+    }
+
+    void Update()
+    {
+        
+        if (gameObject.activeSelf)
+        {
+            var heading = Player.position - GetComponent<Transform>().position;
+            heading.y = 0;
+
+            float offset = GetComponent<Collider>().bounds.size.x * Player.localScale.x;
+
+            if (heading.sqrMagnitude <= inBoundOfInteractions + offset * 1.5)
+            {
+                if (!isRepaired)
+                {
+                    RadialLoadingBar.SetActive(true);
+                    RadialLoadingBar.GetComponent<RadialLoadingBarScript>().SetGameObjectToCall(gameObject);
+                }
+            } else
+            {
+                if (!isRepaired && RadialLoadingBar.activeSelf) RadialLoadingBar.SetActive(false);
+            }
+        }
+
+    }
+}
