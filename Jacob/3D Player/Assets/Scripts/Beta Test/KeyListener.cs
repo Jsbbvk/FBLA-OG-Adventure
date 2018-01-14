5250b75fc1e11e6186bd0e9022a6371e6f361412
Jsbbvk <Jsbbvk@gmail.com>
2018-01-13 20:41:07 -0800

    added builds for v.0.6

--- Jacob/3D Player/Assets/Scripts/Beta Test/KeyListener.cs
+++ Jacob/3D Player/Assets/Scripts/Beta Test/KeyListener.cs
@@ -17,7 +17,7 @@ public class KeyListener : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         
-		if (Input.GetKeyDown(KeyCode.Q))
+		if (Input.GetKeyDown(KeyCode.Tab))
         {
             //toggle profile panel
             
